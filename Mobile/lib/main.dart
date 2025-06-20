725871011b94a37dea8cf7af080dc02e8cd54346
ahmdhdarrz <ahmadhaidar0807@gmail.com>
2025-06-20 21:39:52 +0700

    update branch
    Co-Authored-By: Gemilang Tirto <gemilang2100016062@webmail.uad.ac.id>

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,5 +1,6 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'screens/register/edit_profile.dart';
 import 'screens/register/sign_in.dart';
 import 'screens/register/create_account.dart';
 import 'screens/register/email_verification.dart';
@@ -7,12 +8,18 @@ import 'screens/register/history.dart';
 import 'screens/register/help_and_support.dart';
 import 'screens/register/profile.dart';
 import 'screens/register/openning.dart';
+import 'screens/register/home_screen.dart';
+import 'screens/register/payment_screen.dart';
+import 'screens/register/success_screen.dart';
+import 'screens/register/language.dart';
+import 'screens/register/select_locker.dart';
+
 
 
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -25,7 +32,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Inter',
         useMaterial3: true,
       ),
-      initialRoute: '/sign-in',
+      initialRoute: '/openning',
       routes: {
         '/sign-in': (context) => const SignInScreen(),
         '/create-account': (context) => const CreateAccountScreen(),
@@ -34,6 +41,12 @@ class MyApp extends StatelessWidget {
         '/help_and_support': (context) => const HelpAndSupportScreen(),
         '/profile': (context) => const ProfileScreen(),
         '/openning': (context) => const OpenningScreen(),
+        '/home_screen': (context) => const HomeScreen(),
+        '/payment_screen': (context) => const PaymentScreen(),
+        '/success_screen': (context) => const SuccessScreen(),
+        '/language': (context) => const LanguageScreen(),
+        '/edit_profile': (context) => const EditProfilePage(),
+        '/select_locker': (context) => const SelectLockerScreen(),
 
       },
     );
